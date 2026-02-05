f7bd2213a1a8ff27281227d5449748d6b75bc151
helena6534-hub <helena6534@institutoideia.com>
2026-02-05 08:26:15 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loja virtual</title>
+</head>
+<body>
+    <h1>Minha loja virtual</h1>
+    <p>
+    <?php
+         echo "Ola" ; 
+     ?>
+    </p>
+
+    <?php
+         $nomeLoja = "Loja do 3 ano";
+         echo "Bem vindo a " . $nomeLoja;
+     ?>
+    
+</body>
+</html>
